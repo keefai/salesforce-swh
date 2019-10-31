ee9d5a7a99af52473358794c0dee69ca6d10b613
vabhishek-me <abhishek.k.v76076@gmail.com>
2019-10-31 17:21:28 +0530

    silly mistake

--- src/views/OpportunityInvoice/components/EditableInput/index.js
+++ src/views/OpportunityInvoice/components/EditableInput/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AutosizeInput from 'react-input-autosize';
 import style from './style.module.scss';
 
