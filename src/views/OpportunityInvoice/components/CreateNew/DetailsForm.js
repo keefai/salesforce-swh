32cfc9debb961d788c7d8ffa3da6b0806904dc44
vabhishek-me <abhishek.k.v76076@gmail.com>
2019-11-14 03:23:08 +0530

    form validation and create fix

--- src/views/OpportunityInvoice/components/CreateNew/DetailsForm.js
+++ src/views/OpportunityInvoice/components/CreateNew/DetailsForm.js
@@ -10,12 +10,46 @@ import Checkbox from "@material-ui/core/Checkbox";
 import Map from '../Map';
 import style from './style.module.scss';
 
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
 const DetailsForm = ({
   details,
   handleDetails,
   handleConsent,
   nextStep
 }) => {
+  const [error, setError] = useState(null);
+  const next = () => {
+    // nextStep
+    let err = null;
+    if (details.FirstName === null || details.FirstName.trim() === '') {
+      err = 'First Name is required';
+    } else
+    if (details.LastName === null || details.LastName.trim() === '') {
+      err = 'Last Name is required';
+    } else
+    if (details.PersonEmail === null || details.PersonEmail.trim() === '') {
+      err = 'Email is required';
+    } else
+    if(!validateEmail(details.PersonEmail)) {
+      err = 'Please enter a valid email';
+    } else
+    if (details.Phone === null || details.Phone.trim() === '') {
+      err = 'Mobile Number is required';
+    } else if (details.consent === null || details.consent === false) {
+      err = 'Please click on the checkbox to continue';
+    }
+
+    if (err) {
+      setError(err);
+      setTimeout(() => setError(null), 5000);
+    } else {
+      nextStep();
+    }
+  }
   return (
     <React.Fragment>
       <DialogTitle id="form-dialog-title">
@@ -81,15 +115,13 @@ const DetailsForm = ({
               }}
             /> I accept the <a href="/" target="_blank">Terms of Use</a> and <a href="/" target="_blank">Privacy Policy</a>
           </Grid>
+          <Grid item xs={12}>
+            {error && <span style={{ color: 'red' }}>{error}</span>}
+          </Grid>
         </Grid>
       </DialogContent>
       <DialogActions>
-        {/*
-          <Button onClick={close} color="secondary">
-            Close
-          </Button>
-        */}
-        <Button onClick={nextStep} color="primary">
+        <Button onClick={next} color="primary">
           Next
         </Button>
       </DialogActions>
